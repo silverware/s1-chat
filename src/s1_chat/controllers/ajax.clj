0e7b5ff2bebab1bf9bcee9c72ecaba5ce1844b04
gary.yao <gjl@example.com>
2013-09-03 12:24:19 +0200

    ajax controller

---
+++ src/s1_chat/controllers/ajax.clj
@@ -0,0 +1,27 @@
+(ns s1-chat.controllers.ajax
+  (:require [monger.collection :as mc]
+            [monger.conversion :as mconv]
+            [monger.util :as mu]
+            [s1-chat.models.chat :as chat])
+  (:use compojure.core
+        ring.util.response
+        ))
+
+(defn convert-id [monger-object] 
+  (let [doc-id (.toString (:_id (mconv/from-db-object monger-object true)))]
+    (assoc (mconv/from-db-object monger-object true) :_id doc-id)))
+
+
+(defn user-profile [username]
+  (println (convert-id (mc/find-one "users" {:username username})))
+  (response (dissoc (convert-id (mc/find-one "users" {:username username})) :password)))
+ 
+(defn save-user-profile [{:keys [username] :as user} session-id]
+  (when (chat/valid-session-id? username session-id)
+    (mc/update "users" {:username username} {"$set" (dissoc user :_id)} :upsert false)
+    (response {:success true})))
+
+(def ajax-routes [
+                  (GET "/ajax/user/:username" [username] (user-profile username))
+                  (POST "/ajax/user/" [user session-id] (save-user-profile user session-id))
+                  ])
