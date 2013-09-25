132a3b4ef61a456518807b0d576d7b6a2adb4bd5
gary.yao <gjl@example.com>
2013-09-25 21:41:46 +0200

    removed old leiningen task'

--- tasks/coffeescript/src/leiningen/coffeescript.clj
+++
@@ -1,8 +0,0 @@
-(ns leiningen.coffeescript
-  (:use [clojure.java.shell :only [sh]]))
-
-(defn coffeescript
-  "Compile coffeescript files."
-  [project & args]
-  (let [output-dir (first args) input-dir (second args)]
-    (sh "coffee" (str "--watch --compile -o " output-dir " " input-dir))))
